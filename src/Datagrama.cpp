3509aa24f65643eb330f6e11b2089af1f7332806
Davi <davigiordano2704@gmail.com>
2020-11-01 18:16:07 -0300

    Implement Datagrama

---
+++ src/Datagrama.cpp
@@ -0,0 +1,48 @@
+#include "datagrama.h"
+
+Datagrama::Datagrama(int origem, int destino, int ttl, Segmento *dado)
+{
+    this->origem = origem;
+    this->destino = destino;
+    this->ttl = ttl;
+    this->dado = dado;
+}
+
+Datagrama::~Datagrama()
+{
+    delete dado;
+}
+
+int Datagrama::getOrigem(){
+    return this->origem;
+}
+
+int Datagrama::getDestino(){
+    return this->destino;
+}
+int Datagrama::getTtl(){
+    return this->ttl;
+}
+Segmento* Datagrama::getDado(){
+    return this->dado;
+}
+
+void Datagrama::processar(){
+    this->ttl -= 1;
+}
+
+bool Datagrama::ativo(){
+    return (this->getTtl() > 0);
+}
+
+void Datagrama::imprimir(){
+    cout << "Origem: "
+        << this->origem
+        << ", Destino: "
+        << this->destino
+        << ",TTL: "
+        << this->ttl
+        << ", "
+        << this->dado->getDado()
+        << endl;
+}
