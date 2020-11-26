4af11024673b0ee44047f0141ac14cec4a1dae5c
Davi <davigiordano2704@gmail.com>
2020-11-25 22:42:05 -0300

    Add: Implement getHospedeiros

--- src/Rede.cpp
+++ src/Rede.cpp
@@ -6,14 +6,15 @@ Rede::Rede() {
 }
 
 Rede::~Rede() {
+    //aqui temos que destruir individualmente cada no??
   delete[] this->nos;
 }
 
 // O  método adicionar  deve  adicionar  o No  à Rede.
 // Caso  já  exista  um No  na Rede  com  o  mesmo endereço, jogue um logic_error (da biblioteca padrão) – e não adicione o No.
 void Rede::adicionar(No *no) {
-  for (int i = 0; i < this.quantidadeDeNos; i++) {
-    if (this->nos[i] == no) throw new logic_error("Este no ja foi adicionado"); 
+  for (int i = 0; i < this->quantidadeDeNos; i++) {
+    if (this->nos[i] == no) throw new logic_error("Este no ja foi adicionado");
   }
 
   this->nos[this->quantidadeDeNos] = no;
@@ -24,27 +25,30 @@ void Rede::adicionar(No *no) {
 No* Rede::getNo(int endereco) {
   No *no = NULL;
 
-  for (int i = 0; i < this.quantidadeDeNos; i++) {
+  for (int i = 0; i < this->quantidadeDeNos; i++) {
     if (this->nos[i]->getEndereco() == endereco) no = this->nos[i];
   }
 
   return no;
 }
 
-  // virtual list<Hospedeiro *> *getHospedeiros();
+// Retorna uma list de hospedeiros dentro da Rede
 list<Hospedeiro*>* Rede::getHospedeiros() {
   list<Hospedeiro*> *l = new list<Hospedeiro*>();
 
-  for (int i = 0; i < this.quantidadeDeNos; i++) {
-    // verificar se o no eh um hospedeiro e se sim adicionar na list
-  } 
+  for (int i = 0; i < this->quantidadeDeNos; i++) {
+    Hospedeiro *h = dynamic_cast<Hospedeiro*>(nos[i]);
+    if(h != NULL){
+        l->push_back(h);
+    }
+  }
 
   return l;
 }
 
   // virtual void passarTempo();
 void Rede::passarTempo() {
-  for (int i = 0; i < this.quantidadeDeNos; i++) {
+  for (int i = 0; i < this->quantidadeDeNos; i++) {
     this->nos[i]->processar();
   }
 }
