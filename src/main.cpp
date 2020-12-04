9a5be78387223eea860afee797b5118d6336b00c
Gabriel Mascarenhas <gfmascarenhas@gmail.com>
2020-12-04 15:57:30 -0300

    Fixed errors messages order

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,7 @@
 #include "PersistenciaDeRede.h"
 #include "Fila.h"
 #include "Rede.h"
+#include "Hospedeiro.h"
 using namespace std;
 
 Rede* obterRedeDeArquivo(string file);
@@ -17,8 +18,7 @@ void usarUmNavegador(Rede* rede);
 void passarTempo(Rede* rede);
 void alterarTTl();
 
-int main()
-{
+int main() {
     //obter dados da rede em formato de persistencia
     string fileName;
     cout << "Digite o nome do arquivo: ";
@@ -44,15 +44,14 @@ int main()
                 cout << endl;
                 alterarTTl();
             default:
+                cout << "Opcao invalida" << endl;
                 break;
             }
         } while (tela != 4);
 
     }
-
-
-
 }
+
 //recebe o nome de um arquivo com dados sobre uma rede e tenta criar uma rede, pode pegar um erro do tipo invalid_argument
 Rede* obterRedeDeArquivo(string file){
     PersistenciaDeRede* persistencia = new PersistenciaDeRede();
@@ -84,12 +83,12 @@ int escolherTela(){
 
 void usarUmNavegador(Rede* rede){
     //usar um iterador para percorrer a lista de hospedeiros da Rede,
-    //e em cada hospedeiro percorrer os processos, que é um vector
+    //e em cada hospedeiro percorrer os processos, que ï¿½ um vector
 
     list<Hospedeiro*>* hospedeiros = rede->getHospedeiros();
     list<Hospedeiro*>::iterator i = hospedeiros->begin();
 
-    while(i != hospedeiros ->end()){
+    while(i != hospedeiros->end()){
         cout << "Hospedeiro " << (*i)->getEndereco() << endl;
 
         vector<Processo*>* processos = (*i)->getProcessos();
@@ -112,29 +111,28 @@ void usarUmNavegador(Rede* rede){
     int enderecoDoHospedeiro, portaDoNavegador, enderecoDaPagina, portaDoServidor;
     cout << "Digite o endereco do hospedeiro: ";
     cin >> enderecoDoHospedeiro;
+    No* recuperandoNo = rede->getNo(enderecoDoHospedeiro);
+    Hospedeiro* hospedeiroAtual = dynamic_cast<Hospedeiro*>(recuperandoNo);
+    if(hospedeiroAtual == NULL) {
+        cout << "Endereco invalido" << endl;
+        return;
+    }
+
     cout << "Digite a porta do navegador: ";
     cin >> portaDoNavegador;
-    cout << "Abrir pagina no navegador: ";
+    Processo* recuperandoProcesso = hospedeiroAtual->getProcesso(portaDoNavegador);
+    Navegador* navegadorAtual = dynamic_cast<Navegador*>(recuperandoProcesso);
+    if(navegadorAtual == NULL){
+        cout << "Porta invalida" << endl;
+        return;
+    }
+
+    cout << "Abrir pagina no endereco: ";
     cin >> enderecoDaPagina;
     cout << "Porta do Servidor Web: ";
     cin >> portaDoServidor;
 
-    No* recuperandoNo = rede->getNo(enderecoDoHospedeiro);
-    Hospedeiro* hospedeiroAtual = dynamic_cast<Hospedeiro*>(recuperandoNo);
-
-    if(hospedeiroAtual == NULL){
-        cout << "Endereco invalido" << endl;
-    }else{
-        Processo* recuperandoProcesso = hospedeiroAtual->getProcesso(portaDoNavegador);
-        Navegador* navegadorAtual = dynamic_cast<Navegador*>(recuperandoProcesso);
-
-        if(navegadorAtual == NULL){
-            cout << "Porta invalida" << endl;
-        }else{
-            navegadorAtual->abrir(enderecoDaPagina,portaDoServidor);
-        }
-    }
-
+    navegadorAtual->abrir(enderecoDaPagina,portaDoServidor);
 
 }
 
